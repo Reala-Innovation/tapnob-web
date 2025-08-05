65dd7c3dce50eb907f63cd7e18d62768507190f8
Solomon Emmanuel <soulex04292003@gmail.com>
2025-08-05 10:21:27 +0100

    chore:updated support link

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,9 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export const handleContactClick = () => {
   if (typeof window !== "undefined") {
-    window.open(
-      "https://wa.me/2347063702228?text=Hello%20Tapnob%20Support,%20I%20need%20help",
-      "_blank"
-    );
+    window.open("https://t.me/mubitech23", "_blank");
   }
 };
